e2056b38cb0c0629ef68b2256c7684118dc18c49
IsraelGlez0709 <israelglez812@gmail.com>
2023-07-11 21:37:31 -0600

    Files Updated

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = {
+    ...nextConfig,
+    env: {
+
+    API_KEY: "AIzaSyDZuYaqU7_-aBl3spQ5B802KPFQ3sHbCmk",
+    AUTH_DOMAIN: "dsm-3c-4f349.firebaseapp.com",
+    PROJECT_ID: "dsm-3c-4f349",
+    STORAGE_BUCKET: "dsm-3c-4f349.appspot.com",
+    MESSAGING_SENDER_ID: "806393770037",
+    APP_ID: "1:806393770037:web:431893b9e08541d65dc17c",
+    MEASURMENT_ID: "G-MC2RJLBLRZ"
+    
+    },
+}
